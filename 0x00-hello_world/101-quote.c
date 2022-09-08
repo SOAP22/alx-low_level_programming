823a3c9c1c0e5871c318f14a3cb3e68c0b179db3
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-08 21:24:31 +0000

    learning C is fun

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+
 /**
+ * main - Entry point
  *
- * main - Entry point 
- *
- * Return - Always 1 (error)
- *
+ * Return: Always 1 (Error)
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",59,1,stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
 	return (1);
 }
