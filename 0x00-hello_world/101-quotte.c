88b896cef59679190ce1752274b26b840904d1a5
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-08 21:17:51 +0000

    learning C is fun

---
+++ 0x00-hello_world/101-quotte.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *
+ * main - Entry point 
+ *
+ * Return - Always 1 (error)
+ *
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19",59,1,stderr);
+	return (1);
+}
