4e0415306b97be80ed8ac02206cb90ea8187739f
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-08 19:17:45 +0000

    learning C is fun

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n",sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
-    return(0);
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %d byte(s)\n",sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	return(0);
 }
