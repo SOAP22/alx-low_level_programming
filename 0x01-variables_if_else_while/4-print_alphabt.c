af7245e35a5438737d2671d8242b1488f0b95faf
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-10 23:53:59 +0000

    becoming a real coder as betty style

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,13 +3,15 @@
 /**
  * main - Entry point
  *
- * this program output the aphabet letter in lower case without print 'e' and 'q'
+ * this program output the aphabet letter in lower case 
+ *
+ * without print 'e' and 'q'
  *
  * Return: Always 0 (Succeed)
  */
 int main(void)
 {
-	char alpha [] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'w', 'z'};
+	char alpha [] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
