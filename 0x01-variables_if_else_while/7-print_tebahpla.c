1402a0771713b22db529478425324f78d2754764
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-10 23:46:45 +0000

    becoming a real coder as betty style

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	char alpha [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z'};
+	char alpha [] = "zyxwvutsrqponmlkjihgfedcba"
 	int i;
 
-	for (i = 25; i >= 0; --i)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alpha[i]);
 	}
