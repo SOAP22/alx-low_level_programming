b246a013986949fac22775a6dc1d27be9b9804c1
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-10 23:08:40 +0000

    updating 8-print_bas16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,22 +5,23 @@
  *
  * this program output the base 10 digit
  *
- * Return - Always 0 (Succeed)
+ * Return: Always 0 (Succeed)
  */
 int main(void)
 {
 	int i;
 	char digit[] = {'a', 'b', 'c', 'd', 'e', 'f'};
-        for(i = 0; i < 10; i++)
-        {
-                putchar('0' + i);
-        }
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
 
 	for(i = 0; i < 6; i++)
-        {
-                putchar(digit[i]);
-        }
+	{
+		putchar(digit[i]);
+	}
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
