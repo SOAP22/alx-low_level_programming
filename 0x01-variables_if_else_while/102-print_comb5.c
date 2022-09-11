14b60f3922e1e30893a893e152bdef5b104a9229
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-11 01:30:05 +0000

    becoming a real coder as betty style

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeed)
+ */
+int main(void)
+{
+	int i, j, k, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (m = k+1; m <= 9; m++)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + m);
+					if (i == 9 && j == 8 && k == 9 && m == 9)
+						break;
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
