69e9852a639369c0aa24c4748a43f04d6a2f43f3
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-11 01:36:00 +0000

    becoming a real coder as betty style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = i+1; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = j+1; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
