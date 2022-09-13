71aae27583344fda923c5019ab6faf3e5d597d6e
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-13 21:21:59 +0000

    starting

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succeed)
+ */
+int main(void)
+{
+	char c[] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(c[i]);
+	return (0);
+}
