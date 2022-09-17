51746a45854aaf8b376adb742313acc4ea621616
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-17 03:03:21 +0000

    next stage

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
  * Return: 0 (Succeed)
@@ -11,7 +11,11 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
+                {
+                        printf("FizzBuzz ");
+                }
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -19,13 +23,9 @@ int main(void)
 		{
 			printf("Buzz ");
 		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
 		else
 		{
-			printf("%i", i);
+			printf("%i ", i);
 		}
 		i++;
 	}
