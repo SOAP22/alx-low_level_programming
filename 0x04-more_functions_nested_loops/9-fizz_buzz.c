14c7784f957c057051ff835ca369fa9344bf3f56
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-17 03:08:14 +0000

    next stage

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-                {
-                        printf("FizzBuzz ");
-                }
+		{
+			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
