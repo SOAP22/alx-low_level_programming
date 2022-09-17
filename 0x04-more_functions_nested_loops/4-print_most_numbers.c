08d6896a479d7c9f349493f95155334a886d1e1d
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-17 02:33:51 +0000

    next stage

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	int i = -1;
+	int i = 0;
 
 	while (i <= 9)
 	{
-		if ( i == 2 || i == 4)
-			continue;
+		if ( i != 2 || i != 4)
+			_putchar('0' + i);
 		i++;
-		_putchar('0' + i);
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
