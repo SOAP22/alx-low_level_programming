0912f6c82c5db318d52d8199fc1f2f8ed3957808
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-18 15:50:55 +0000

    next step

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,13 +12,13 @@ void print_triangle(int n)
 	int i, j, k;
 
 	if (n > 0)
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = n; j > i; j-- )
+			for (j = n; j >= i; j-- )
 			{
 				_putchar(' ');
 			}
-			for ( k = 0; k < i; k++)
+			for ( k = 1; k <= i; k++)
 				_putchar('#');
 			_putchar('\n');
 		}
