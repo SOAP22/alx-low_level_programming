640ffb7e7e2141f4bf676dc7db6bb36b6f4dfdc7
Ogou Salomon <ogoupierre409@gmail.com>
2022-09-19 23:55:38 +0000

    pointer_arrays_string

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < 13; i++)
+		printf("%c", rand() % 256);
+	return (0);
+}
